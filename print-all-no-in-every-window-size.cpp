9e3b1187d03904e51689ef69f8bc26f184b05a89
vanshdubey2445-lang <vanshdubey2445@gmail.com>
2026-05-18 22:44:53 +0530

    Implement solution to print all numbers in every window of size k

---
+++ print-all-no-in-every-window-size.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void display(queue<int>q)
+{
+    while(!q.empty())
+    {
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+
+    cout<<endl;
+}
+int main()
+{
+    int arr[]={3,6,2,7,8,11};
+    int n =6;
+    int k=3;
+
+    queue<int>q;
+    for(int i=0;i<k-1;i++)
+    q.push(arr[i]);
+
+    for(int i=k-1;i<n;i++)
+    {
+        q.push(arr[i]);
+        display(q);
+        q.pop();
+    }
+
+    return 0;
+}
