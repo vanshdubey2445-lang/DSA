1d6afcaf5c97d2725ca966173bf4f9175ec61446
vanshdubey2445-lang <vanshdubey2445@gmail.com>
2026-06-29 23:35:40 +0530

    Add implementation of heap sort with heapify, build max heap, and print functionalities

---
+++ heap-sort.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+void heapify(int arr[],int index,int size)
+{
+    int largest=index;
+    int left= 2*index+1;
+    int right=2*index+2;
+
+    if(left<size && arr[left]>arr[largest])
+    largest=left;
+    if(right<size && arr[right]>arr[largest])
+    largest=right;
+
+    if(largest!=index)
+    {
+        swap(arr[index],arr[largest]);
+        heapify(arr,largest,size);
+    }
+
+
+}
+
+void buildmaxheap(int arr[],int n)
+{
+    //step down
+    for(int i=n/2-1;i>=0;i--)
+    {
+        heapify(arr,i,n);
+    }
+}
+
+void printheap(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";      
+
+    cout<<endl; 
+}
+
+void sortarray(int arr[],int n)
+{
+    for(int i=n-1;i>0;i--)
+    {
+        swap(arr[i],arr[0]);
+        heapify(arr,0,i);
+    }
+}
+
+int main()
+{
+    int arr[]={10,3,8,9,5,13,18,14,11,70};
+     buildmaxheap(arr,10);
+     sortarray(arr,10);
+     printheap(arr,10);
+
+}
