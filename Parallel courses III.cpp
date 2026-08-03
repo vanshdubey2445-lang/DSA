545a029d6b0b4d98ef4651de1a50476f29ad26a9
vanshdubey2445-lang <vanshdubey2445@gmail.com>
2026-08-03 22:12:07 +0530

    Add implementation to calculate the minimum time needed to complete all courses based on prerequisites

---
+++ Parallel courses III.cpp
@@ -0,0 +1,83 @@
+// You are given an integer n, which indicates that there are n courses labeled from 1 to n. You are also given a 2D integer array relations where relations[j] = [prevCoursej, nextCoursej] denotes that course prevCoursej has to be completed before course nextCoursej (prerequisite relationship). Furthermore, you are given a 0-indexed integer array time where time[i] denotes how many months it takes to complete the (i+1)th course.
+
+// You must find the minimum number of months needed to complete all the courses following these rules:
+
+// You may start taking a course at any time if the prerequisites are met.
+// Any number of courses can be taken at the same time.
+// Return the minimum number of months needed to complete all the courses.
+
+// Note: The test cases are generated such that it is possible to complete every course (i.e., the graph is a directed acyclic graph).
+
+ 
+
+// Example 1:
+
+
+// Input: n = 3, relations = [[1,3],[2,3]], time = [3,2,5]
+// Output: 8
+// Explanation: The figure above represents the given graph and the time required to complete each course. 
+// We start course 1 and course 2 simultaneously at month 0.
+// Course 1 takes 3 months and course 2 takes 2 months to complete respectively.
+// Thus, the earliest time we can start course 3 is at month 3, and the total time required is 3 + 5 = 8 months.
+// Example 2:
+
+
+// Input: n = 5, relations = [[1,5],[2,5],[3,5],[3,4],[4,5]], time = [1,2,3,4,5]
+// Output: 12
+// Explanation: The figure above represents the given graph and the time required to complete each course.
+// You can start courses 1, 2, and 3 at month 0.
+// You can complete them after 1, 2, and 3 months respectively.
+// Course 4 can be taken only after course 3 is completed, i.e., after 3 months. It is completed after 3 + 4 = 7 months.
+// Course 5 can be taken only after courses 1, 2, 3, and 4 have been completed, i.e., after max(1,2,3,7) = 7 months.
+// Thus, the minimum time needed to complete all the courses is 7 + 5 = 12 months.
+
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {  //bfs traversal
+        
+        vector<int>adj[n];
+
+        for(int i=0;i<relations.size();i++)
+        {
+            adj[relations[i][0]-1].push_back(relations[i][1]-1);
+        }
+
+        vector<int>indeg(n,0);
+        for(int i=0;i<n;i++)
+        for(int j=0;j<adj[i].size();j++)
+        indeg[adj[i][j]]++;
+
+        queue<int>q;
+        vector<int>coursetime(n,0);
+
+        for(int i=0;i<n;i++)
+        if(!indeg[i])
+        q.push(i);
+
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+
+            for(int j=0;j<adj[node].size();j++)
+            {
+                indeg[adj[node][j]]--;
+                if(!indeg[adj[node][j]])
+                q.push(adj[node][j]);
+
+            coursetime[adj[node][j]]=max(coursetime[adj[node][j]],coursetime[node]+time[node]);
+            }
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++)
+        ans=max(ans,coursetime[i]+time[i]);
+
+        return ans;
+    }
+};
